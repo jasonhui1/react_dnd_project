5e8aacb275a723014cfa555c0604a73034468260
jasonhui1 <jason903813@gmail.com>
2023-04-13 22:35:16 +0800

    change code to useContext

--- frontend/src/Components/Board/Section.tsx
+++ frontend/src/Components/Board/Section.tsx
@@ -4,34 +4,29 @@ import { useDrop } from "react-dnd";
 import Card, { PassProp } from './Card';
 import { Todo } from '../../types/Todo';
 import { AddIcon, DeleteIcon } from '@chakra-ui/icons'
+import { useBoardContext } from '../../context/board';
 
 
 interface SectionProps {
     properties: Section,
     positionIndex: number,
-    handleDrop: (_id: string, prevSectionIndex: number, index: number,) => void
-    onHoverSwapCard: (dragCardId: string, hoverIndex: number, prevSectionIndex: number, sectionIndex: number) => void
-    onDropSwapCard: (dragCardId: string, hoverIndex: number, sectionIndex: number) => void
-    onClickDeleteSection: (id: string) => void
-    onClickAddCard: (sectionId: string, title: string) => void
-    onClickDeleteCard: (sectionId: string, cardId: string) => void
 }
 
 interface DropSectionProps {
     positionIndex: number,
-    handleDrop: (_id: string, prevSectionIndex: number, index: number,) => void
     children: React.ReactNode
-
 }
 
-function DropSection({ positionIndex, handleDrop, children }: DropSectionProps) {
+function DropSection({ positionIndex, children }: DropSectionProps) {
+
+    const { handleDrop } = useBoardContext();
 
     //Drop to the section->Add it
     const [{ isOver }, drop] = useDrop({
         accept: "todo",
         drop: (item: PassProp, monitor) => {
             const didDrop = monitor.didDrop()
-            if(didDrop) return //Drop on cards already
+            if (didDrop) return //Drop on cards already
 
             const { _id, sectionIndex } = item
             const prevSectionIndex = sectionIndex
@@ -59,12 +54,13 @@ function DropSection({ positionIndex, handleDrop, children }: DropSectionProps) 
     );
 }
 
-export default function SectionComponent({ properties, positionIndex, handleDrop, onHoverSwapCard, onDropSwapCard, onClickDeleteSection, onClickAddCard, onClickDeleteCard }: SectionProps) {
+export default function SectionComponent({ properties, positionIndex }: SectionProps) {
 
     const [title, setTitle] = useState('')
+    const {onClickDeleteSection, onClickAddCard} = useBoardContext();
 
     return (
-        <DropSection positionIndex={positionIndex} handleDrop={handleDrop}>
+        <DropSection positionIndex={positionIndex}>
             <Flex direction='column' justify='space-between' h='full' p='3'>
                 <Box>
                     <Flex gap='2' mt='5' mb='1' align='center'>
@@ -73,7 +69,7 @@ export default function SectionComponent({ properties, positionIndex, handleDrop
                         <DeleteIcon ml='auto' onClick={() => onClickDeleteSection(properties._id)} mt='' color='white' />
                     </Flex>
                     <Divider my={5} />
-                    <CardList properties={properties} positionIndex={positionIndex} onHoverSwapCard={onHoverSwapCard} onDropSwapCard={onDropSwapCard} onClickDeleteCard={onClickDeleteCard} />
+                    <CardList properties={properties} positionIndex={positionIndex}/>
 
                     <Flex gap='2' mt='5' mb='1' align='center'>
 
@@ -99,20 +95,17 @@ export interface Section {
 interface CardListsProps {
     properties: Section,
     positionIndex: number,
-    onHoverSwapCard: (dragCardId: string, hoverIndex: number, prevSectionIndex: number, sectionIndex: number) => void
-    onDropSwapCard: (dragCardId: string, hoverIndex: number, sectionIndex: number) => void
-    onClickDeleteCard: (sectionId: string, cardId: string) => void
 }
 
-function CardList({ properties, positionIndex, onHoverSwapCard, onDropSwapCard, onClickDeleteCard }: CardListsProps) {
+function CardList({ properties, positionIndex}: CardListsProps) {
+    const {onClickDeleteCard } = useBoardContext();
+
     return (
         <Stack gap={1}>
             {
                 properties.cards.map((card: Todo, index) => {
                     return (
                         <Card key={card._id} properties={card} positionIndex={index} sectionIndex={positionIndex}
-                            onHover={onHoverSwapCard}
-                            onDrop={onDropSwapCard}
                             onDelete={() => onClickDeleteCard(properties._id, card._id)} />
 
                     )
